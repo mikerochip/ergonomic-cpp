c0e18fe68ec17a5b3e819f195c7321d5cd9d39f4
Mike Schweitzer <mschweitzer@gmail.com>
2022-02-26 23:25:38 -0800

    Properly specify Configuration to conan install in build.ps1

--- build.ps1
+++ build.ps1
@@ -19,7 +19,7 @@ $BuildDir = "build-$ConfigDirName"
 New-Item -Type Directory -Force $BuildDir
 Set-Location $BuildDir
 
-conan install .. --build=missing
+conan install .. -s build_type=$ConfigTypeName --build=missing
 # the source dir is the parent since that's where CMakeLists.txt is
 cmake -DCMAKE_BUILD_TYPE=$ConfigTypeName -S .. -B .
 make all
