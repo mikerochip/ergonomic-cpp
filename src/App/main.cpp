caf8a501e5a10bc7f9af227e1c4812e1cfb588d2
Mike Schweitzer <mschweitzer@gmail.com>
2022-06-18 16:57:28 -0700

    Add CLI11 CLI parser with basic usage

--- src/App/main.cpp
+++ src/App/main.cpp
@@ -1,21 +1,19 @@
 #include <string>
+#include "CLI/CLI.hpp"
 #include "fmt/core.h"
-#include "Poco/MD5Engine.h"
-#include "Poco/DigestStream.h"
 #include "MyApp/Foo.h"
 #include "MyLibrary/Foo.h"
+#include "Poco/MD5Engine.h"
+#include "Poco/DigestStream.h"
 
 using namespace fmt;
 using namespace std;
 
-string GeneratePocoMd5Hex()
-{
-    Poco::MD5Engine md5;
-    Poco::DigestOutputStream ds(md5);
-    ds << "abcdefghijklmnopqrstuvwxyz";
-    ds.close();
-    return Poco::DigestEngine::digestToHex(md5.digest());
- }
+#define ERGONOMIC_CPP_APP_NAME "1.0.0"
+#define ERGONOMIC_CPP_APP_VERSION "1.0.0"
+
+void BuildCommandParser(CLI::App& app);
+string GeneratePocoMd5Hex();
 
 int main(int argc, char** argv)
 {
@@ -29,5 +27,39 @@ int main(int argc, char** argv)
 
     string md5Hex = GeneratePocoMd5Hex();
     print("Poco::MD5::Hex={}\n", md5Hex);
+
+    CLI::App app;
+    BuildCommandParser(app);
+    CLI11_PARSE(app, argc, argv);
+
+    auto&& options = app.get_options();
+    print("Options={}\n", options.size());
+    for (auto&& option : options)
+        print("{}\n", option->get_name());
     return 0;
+}
+
+void BuildCommandParser(CLI::App& app)
+{
+    app.name(ERGONOMIC_CPP_APP_NAME);
+    app.set_version_flag("--version", ERGONOMIC_CPP_APP_VERSION);
+    app.allow_extras(true);
+
+    int count = 0;
+    app.add_option("-c,--count", count, "Counter");
+
+    int v = 0;
+    app.add_flag("--flag", v, "Some flag that can be passed multiple times");
+
+    double value = 0.0;  // = 3.14;
+    app.add_option("-d,--double", value, "Some Value");
+}
+
+string GeneratePocoMd5Hex()
+{
+    Poco::MD5Engine md5;
+    Poco::DigestOutputStream ds(md5);
+    ds << "abcdefghijklmnopqrstuvwxyz";
+    ds.close();
+    return Poco::DigestEngine::digestToHex(md5.digest());
 }
